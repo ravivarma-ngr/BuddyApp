a7f2ef0aa896baaf6cafc3a660e6dcbd070dd1ba
Ravivarma <ravikumarvarma_nagaraju@epam.com>
2022-07-26 11:53:57 +0530

    buddy app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,DashboardComponent]
 })
 export class AppModule { }
