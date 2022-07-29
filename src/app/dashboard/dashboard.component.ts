c5181d97a66895677ce70a3578c00ce671a74466
Sreehari Chinta <Sreehari_Chinta@epam.com>
2022-07-29 17:28:17 +0530

    Added base code to show dummy data

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Transaction } from '../transaction';
 
 @Component({
   selector: 'dashboard',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  public transactions: Transaction[]
+  constructor() {
+    this.transactions = [
+      {
+        transactionId: "1",
+        transactionName: "Salary",
+        transactionCategory: "Salary",
+        transactionDate:  new Date(),
+        transactionType: "Income",
+        transactionAmount: 100000
+      },
+      {
+        transactionId: "2",
+        transactionName: "Eating out",
+        transactionCategory: "Food",
+        transactionDate:  new Date(),
+        transactionType: "Expense",
+        transactionAmount: 540
+      },
+      {
+        transactionId: "3",
+        transactionName: "Movies",
+        transactionCategory: "Entertainment",
+        transactionDate:  new Date(),
+        transactionType: "Expense",
+        transactionAmount: 1000
+      },
+    ]
+   }
 
   ngOnInit(): void {
   }
+
 
 }
---
+++ src/app/transaction.ts
@@ -0,0 +1,8 @@
+export interface Transaction {
+  transactionName: string;
+  transactionType: string;
+  transactionDate: Date;
+  transactionCategory: string;
+  transactionId: string;
+  transactionAmount: number
+}
